b89d3e7910d42f831d7a6c85fbb835bfa34a978f
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-02-15 11:35:39 +0100

    add reVerification

--- controllers/auth/reVerification.js
+++ controllers/auth/reVerification.js
@@ -1,10 +1,11 @@
 const { User } = require("../../models");
 const { HttpError, nodemailerFn } = require("../../helpers");
 
-const reVerification = (req, res, next) => {
+const reVerification = async (req, res, next) => {
   const { email } = req.body;
-  console.log("email", email);
-  const user = User.findOne({ email });
+
+  const user = await User.findOne({ email });
+  console.log("user in reVerification:", user);
 
   if (!user) {
     throw HttpError(401, "User not found");
@@ -13,12 +14,32 @@ const reVerification = (req, res, next) => {
     throw HttpError(401, "Email already verify");
   }
 
-  nodemailerFn(user.verificationToken, email);
+  try {
+    // user.verify = true;
+    // user.verificationToken = "";
 
-  res.status(201).json({
-    email,
-    [user.verificationToken]: [user.verificationToken],
-  });
+    // console.log("user after reVerification:", user);
+
+    // await user.save();
+
+    await User.findByIdAndUpdate(user._id, {
+      verify: true,
+      verificationToken: "",
+    });
+
+    nodemailerFn(user.verificationToken, email);
+
+    res.status(201).json({
+      email,
+      [user.verificationToken]: [user.verificationToken],
+    });
+  } catch (error) {
+    if (error.message.includes("E11000") || error.message.code === 11000) {
+      // 11000 - помилка mongoDB яка говорить про наявність дублікату даних у БД
+      throw HttpError(409, "Email in use");
+    }
+    throw error;
+  }
 };
 
 module.exports = reVerification;
